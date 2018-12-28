087340459a54c8ca11e93036327452044ebc3935
Kjeeeee <32417093+Kjeeeee@users.noreply.github.com>
2018-12-28 11:30:42 +0300

    Add files via upload

---
+++ Howling-Software-V2-master/Howling_Software/Globals.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Howling_Software
+{
+    
+    class Globals
+    {
+        public static string AdminName = "xxd"; // Admins separated by space
+        public static string loggedoff = "false"; // don´t change
+        public static string username = ""; //don´t change
+        public static string cheat1 = "Howling Cheat CSGO"; // CheatName in Listbox
+        public static string secret_key = "4HcWMviwFHVjQINSBuMo08GpLNr80gQdx2HnAnZnMKdIu"; //your Secret Code
+        public static string DownLink = "https://github.com/Kjeeeee/dll/blob/master/wanhedaSDK.dll"; //your download link
+        public static string CheatName = "wanhedaSDK.dll"; // Cheat name
+    }
+    
+}
